11808f565d4828ec8f1d7315f4a41d522957710d
alwaysshuvo <mds126258@gmail.com>
2025-12-17 15:25:52 +0600

    updated

--- clients/components/BestSelling.jsx
+++ clients/components/BestSelling.jsx
@@ -4,32 +4,35 @@ import ProductCard from './ProductCard'
 import { useSelector } from 'react-redux'
 
 const BestSelling = () => {
+  const displayQuantity = 8
 
-    const displayQuantity = 8
-    const products = useSelector(state => state.product.list)
+  const products = useSelector(state =>
+    Array.isArray(state.product.list) ? state.product.list : []
+  )
 
-    // Safe layout + safe rating sort
-    const sortedProducts = products.slice().sort((a, b) => {
-        const ratingA = a?.rating?.length || 0
-        const ratingB = b?.rating?.length || 0
-        return ratingB - ratingA
+  const sortedProducts = products
+    .slice()
+    .sort((a, b) => {
+      const ratingA = a?.rating?.length || 0
+      const ratingB = b?.rating?.length || 0
+      return ratingB - ratingA
     })
 
-    return (
-        <div className='px-6 my-30 max-w-6xl mx-auto'>
-            <Title
-                title='Best Selling'
-                description={`Showing ${products.length < displayQuantity ? products.length : displayQuantity} of ${products.length} products`}
-                href='/shop'
-            />
+  return (
+    <div className='px-6 my-30 max-w-6xl mx-auto'>
+      <Title
+        title='Best Selling'
+        description={`Showing ${Math.min(displayQuantity, products.length)} of ${products.length} products`}
+        href='/shop'
+      />
 
-            <div className='mt-12 grid grid-cols-2 sm:flex flex-wrap gap-6 xl:gap-12'>
-                {sortedProducts.slice(0, displayQuantity).map((product, index) => (
-                    <ProductCard key={index} product={product} />
-                ))}
-            </div>
-        </div>
-    )
+      <div className='mt-12 grid grid-cols-2 sm:flex flex-wrap gap-6 xl:gap-12'>
+        {sortedProducts.slice(0, displayQuantity).map(product => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default BestSelling
